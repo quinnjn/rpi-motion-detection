4edfc634224cd0a1dfbec7a6ce19404c42af923a
quinnjn <quinnjn@gmail.com>
2016-03-05 04:19:16 +0000

    Making videos from captures

---
+++ make-video.py
@@ -0,0 +1,60 @@
+import cv2
+import os
+import pprint 
+
+WORKING_DIR = './videos'
+CAPTURES_DIR = './captures'
+TIME_THRESHOLD_SECONDS = 5
+
+def mkdir(path):
+ if not os.path.exists(path):
+  os.makedirs(path)
+
+def frameFilepath(framePath):
+ return os.path.join(CAPTURES_DIR, framePath)
+ 
+def filepath(grouping):
+ return os.path.join(WORKING_DIR, grouping)
+
+def getListOfCaptures():
+ captures =[f for f in os.listdir(CAPTURES_DIR) if '.jpg' in f]
+ captures.sort()
+ return captures
+
+def getCaptureGroupings():
+ groupings = {} 
+ captures = getListOfCaptures()
+ lastDate = ""
+ lastTime = 0 
+ lastGroup = ""
+ for c in captures:
+  (date, time, seconds) = c.split('.')[0].split('-')
+ 
+  if int(time) <= TIME_THRESHOLD_SECONDS + lastTime:
+   groupings[lastGroup].append(c)
+  else:
+   lastGroup = '-'.join([date, time])
+   groupings[lastGroup] = [c]
+  lastTime = int(time)
+
+ return groupings
+
+def lazyVideoWriter(group, frame):
+ path = filepath(group) + '.avi'
+ (h,w,layers) = frame.shape
+ fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+ return cv2.VideoWriter(path, fourcc, 1, (w, h))
+
+mkdir(WORKING_DIR)
+
+for group, framePaths in getCaptureGroupings().iteritems():
+ video = None
+ path = filepath(group)
+ for framePath in framePaths:
+  frame = cv2.imread(frameFilepath(framePath))
+  if not video:
+   video = lazyVideoWriter(group, frame)
+  video.write(frame)
+ print video
+ video.release()
+
